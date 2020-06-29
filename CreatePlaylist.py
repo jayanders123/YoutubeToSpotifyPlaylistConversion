e9bfc3a55d2cb4c2d156cfb4ba88f75c14c86b2e
Joshua <j5anderson@hotmail.co.uk>
2020-06-29 23:53:39 +0100

    Added Youtube client functionality - can now retrieve list of songs in chosen playlist

--- CreatePlaylist.py
+++ CreatePlaylist.py
@@ -5,35 +5,41 @@ import os
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
+import youtube_dl
 
 from spotify_secrets import SPOTIFY_USER_ID,spotify_token
 
 class CreatePlaylist:
-    def _init_(self):
-        self.youtube_client = self.get_youtube_client()
+    youtube_playlist_id = "PLk0vAs4YY3u_tlo96dpuVeHb9oex3a1NJ"
+    all_youtube_song_info = {}
 
-    def get_youtube_client(self):
-        """ Log Into Youtube, Copied from Youtube Data API """
+    def _init_(self):
+        pass
+
+    def get_youtube_videos(self, playlist = youtube_playlist_id):
         # Disable OAuthlib's HTTPS verification when running locally.
         # *DO NOT* leave this option enabled in production.
-        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
 
         api_service_name = "youtube"
         api_version = "v3"
         client_secrets_file = "client_secret.json"
 
         # Get credentials and create an API client
-        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
         flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-            client_secrets_file, scopes)
+            client_secrets_file, scopes="https://www.googleapis.com/auth/youtube.readonly")
         credentials = flow.run_console()
-
-        # from the Youtube DATA API
-        youtube_client = googleapiclient.discovery.build(
+        youtube = googleapiclient.discovery.build(
             api_service_name, api_version, credentials=credentials)
-        pdb.set_trace()
-        return youtube_client
 
+        request = youtube.playlistItems().list(
+            part="contentDetails",
+            playlistId= playlist
+        )
+        pdb.set_trace()
+        response = request.execute()
+        return response
 
     def get_liked_videos(self):
         pass
@@ -74,9 +80,10 @@ class CreatePlaylist:
 
 
     def add_songs_to_playlist(self):
-        pass
+        self.get_youtube_videos()
 
 
-instance = CreatePlaylist()
 
-instance.get_youtube_client()
+if __name__ == '__main__':
+    cp = CreatePlaylist()
+    cp.add_songs_to_playlist()
